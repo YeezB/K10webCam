9bd028ab7342b8c8b6d1815be45dfd13f62543f0
YeezB <roger961130@icloud.com>
2025-06-17 17:04:48 +0800

    Add English blocks

--- arduinoC/main.ts
+++ arduinoC/main.ts
@@ -2,7 +2,7 @@
     //% color="#aa00ff" iconWidth=50 iconHeight=40
     namespace yeezb{
         
-    //% block="网络摄像头[k]" blockType="command"
+    //% block="webcamera[k]" blockType="command"
     //% k.shadow="dropdown" k.options="k" 
     export function myBlock0(parameter: any, block: any) {
     let k= parameter.k.code
